0bac1d64ca1e163a2085e0a501c44d43c40cb6a6
ivan@duraflame <ivan@duraflame.(none)>
2009-08-04 22:59:29 -0700

    link_ids are now automatically generated on nada save

--- app/models/nada.rb
+++ app/models/nada.rb
@@ -1,2 +1,10 @@
+require 'digest/sha1'
+
 class Nada < ActiveRecord::Base
+    before_save :generate_link_tag
+
+    protected
+	def generate_link_tag
+	    self.link_tag = Digest::SHA1.hexdigest("--#{image_url}--#{image_filename}--#{text}--").slice(0..8)
+	end
 end
